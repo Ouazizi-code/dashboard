912d37e41335e611b8e6bb943a71b2e7a72c42d1
ouazizi <ouazizi2028@gmail.com>
2024-05-02 00:14:33 +0100

    setup project

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let shownBtn = document.getElementById('shown-btn');
+let links = document.querySelector('.links');
+let btn = document.querySelector('.btn');
+let hidenBtn = document.createElement('button');
+    hidenBtn.textContent = 'close';
+
+shownBtn.addEventListener('click',function(){
+    links.classList.toggle('show');
+    /*this.onclick = function(){
+        links.classList.add('hide')
+    }*/
+    
+}) 
