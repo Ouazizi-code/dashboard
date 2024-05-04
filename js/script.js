ef92273e6eb1f7dc8b3e3eee88972aa6453777dc
ouazizi <ouazizi2028@gmail.com>
2024-05-04 19:47:12 +0100

    up js file

--- js/script.js
+++ js/script.js
@@ -1,24 +1,14 @@
-/*let shownBtn = document.getElementById('shown-btn');
-let links = document.querySelector('.links');
-let btn = document.querySelector('.btn');
-let hidenBtn = document.createElement('button');
-    hidenBtn.textContent = 'close';
 
-shownBtn.addEventListener('click',function(){
-    links.classList.toggle('show');
-    /*this.onclick = function(){
-        links.classList.add('hide')
-    }*/
-    
-/*}) *///
+// ==== lets start coding the header ====//
 
-console.log('hassan')
 
 let links = document.querySelector('.new-links')
 let burger_btn = document.querySelector('.burger_btn')
-console.log(burger_btn,links)
+
 
 burger_btn.addEventListener ('click', function(){
     links.classList.toggle('show-links');
 })
+
+//==== end header ==== //
 
